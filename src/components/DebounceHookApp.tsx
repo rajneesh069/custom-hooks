18a1b03fae219a8d1d731110cfe83949fa0140e1
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-05 10:34:06 +0530

    modify the app to include and showcase all the features

--- src/components/DebounceHookApp.tsx
+++ src/components/DebounceHookApp.tsx
@@ -1,24 +1,58 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
+// import { useEffect } from "react";
 import { useDebounce } from "../hooks/useDebounce";
 
 export default function Debounce() {
   const [input, setInput] = useState<string>("");
-  const debouncedValue = useDebounce(input, 6000);
+  const [delay, setDelay] = useState<number | string>("");
+  const [delayInHook, setDelayInHook] = useState<number>(0);
+  const debouncedValue = useDebounce(input, delayInHook);
 
-  useEffect(() => {
-    console.log(debouncedValue);
-  }, [debouncedValue]);
+  // can do an API call here
+  // useEffect(() => {
+  //   console.log(debouncedValue);
+  // }, [debouncedValue]);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
+
+  const handleDelayChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setDelay(Number(e.target.value));
+  };
+
   return (
-    <div>
-      <input
-        placeholder="Search..."
-        onChange={handleInputChange}
-        value={input}
-      />
+    <div
+      style={{ display: "flex", flexDirection: "column", gap: 4, margin: 5 }}
+    >
+      <div style={{ display: "flex", gap: 2 }}>
+        <input
+          type="number"
+          placeholder="Delay in MS"
+          value={delay}
+          onChange={handleDelayChange}
+        />
+        <button
+          onClick={() => {
+            setDelayInHook(delay as number);
+          }}
+        >
+          Set Delay
+        </button>
+      </div>
+      <div>
+        <input
+          placeholder="Search..."
+          onChange={handleInputChange}
+          value={input}
+        />
+      </div>
+      <div style={{ display: "flex" }}>
+        <p>Debounced Value(delayed after a given time) : &nbsp;</p>
+        <p style={{ border: "2px solid black", minWidth: "100px" }}>
+          {debouncedValue as string}
+        </p>
+      </div>
     </div>
   );
 }
